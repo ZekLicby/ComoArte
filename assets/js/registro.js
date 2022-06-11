25f5e3cfdee13326d78cfac43b5771965eb319ee
Carlos Lins <carloslinsalbertoa@gmail.com>
2022-06-11 14:33:27 -0300

    Primeiro commit (final do projeto)

---
+++ assets/js/registro.js
@@ -0,0 +1,7 @@
+function guardarValores(){
+    const nome = document.getElementsByClassName('campo-form'[0])
+    const tipoPessoa = document.getElementsByClassName('campo-radio')
+    const CPFouCNPJ = document.getElementsByClassName('campo-form'[1])
+    const email = document.getElementsByClassName('campo-form'[2])
+    const senha = document.getElementsByClassName('campo-form'[3])
+}
